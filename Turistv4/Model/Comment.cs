72608d3a5bccf441978e1bcf1ce44837236adf0c
encai <ninjalab@hotmail.com>
2014-11-20 13:33:07 +0100

    tilføjet til test

--- Turistv4/Model/Comment.cs
+++ Turistv4/Model/Comment.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace Turistv4.Model
 {
+  //  [Serializable]
     class Comment
     {
         private int _comRating;
---
+++ UnitTestProjectt/UnitTest1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.CodeDom;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProjectt
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Testrating1()
+        {
+            Comment comment = new Comment("asdfgbejdt", 3);
+
+                try
+                {
+                    comment.rating(6);
+                    Assert.Fail();
+                }
+                catch (ArgumentException ae)
+                {
+                    
+                    // ok
+                }     
+        }
+        [TestMethod]
+        public void Testrating2()
+        {
+            Comment comment = new Comment("asdfgbejdt", 3);
+
+            try
+            {
+                comment.rating(0);
+                Assert.Fail();
+            }
+            catch (ArgumentException ae)
+            {
+
+                // ok
+            }
+        }
+        [TestMethod]
+        public void Testcomment()
+        {
+            Comment comment = new Comment("asdfgbejdt", 3);
+
+            try
+            {
+                comment.rating(6);
+                Assert.Fail();
+            }
+            catch (ArgumentException ae)
+            {
+
+                // ok
+            }
+        }
+    }
+}
