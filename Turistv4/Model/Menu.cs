9e355b7f059cb2d832d93c0add419a31db23ef9e
encai <ninjalab@hotmail.com>
2014-11-20 09:38:30 +0100

    menu class lavet

--- Turistv4/Model/Menu.cs
+++ Turistv4/Model/Menu.cs
@@ -8,5 +8,33 @@ namespace Turistv4.Model
 {
     class Menu
     {
+        private string _MenuName;
+        private string _MenuPrice;
+        private string _MenuInfo;
+        private string _menuImage;
+
+        public string MenuName
+        {
+            get { return _MenuName; }
+            set { _MenuName = value; }
+        }
+
+        public string MenuPrice
+        {
+            get { return _MenuPrice; }
+            set { _MenuPrice = value; }
+        }
+
+        public string MenuInfo
+        {
+            get { return _MenuInfo; }
+            set { _MenuInfo = value; }
+        }
+
+        public string MenuImage
+        {
+            get { return _menuImage; }
+            set { _menuImage = value; }
+        }
     }
 }
