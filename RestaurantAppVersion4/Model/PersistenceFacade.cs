e476282d4413d09ee09ab9cebfa1be6e58e52515
Rasmus Friis <rasmuswfriis@gmail.com>
2014-11-25 11:48:04 +0100

    PF1

--- RestaurantAppVersion4/Model/PersistenceFacade.cs
+++ RestaurantAppVersion4/Model/PersistenceFacade.cs
@@ -8,33 +8,33 @@ using System.Threading.Tasks;
 using System.Xml.Serialization;
 using Windows.Storage;
 using Newtonsoft.Json;
-using Turistv4.Model;
+using RestaurantAppVersion4.Model;
 
 namespace WindowsAppsSerializeTest
 {
     class PersistenceFacade
     {
-        private static string jsonFileName = "PersonsAsJson.dat";
-        
-        public static async void SavePersonsAsJsonAsync(ObservableCollection<Person> persons)
+        private static string jsonFileName = "Comments.dat";
+
+        public static async void SaveCommentsAsJsonAsync(ObservableCollection<Comment> Comments)
         {
-            string personsJsonString = JsonConvert.SerializeObject(persons);
-            SerializePersonsFileAsync(personsJsonString, jsonFileName);
+            string CommentsJsonString = JsonConvert.SerializeObject(Comments);
+            SerializePersonsFileAsync(CommentsJsonString, jsonFileName);
         }
 
-        public static async Task<List<Comment>> LoadPersonsFromJsonAsync()
+        public static async Task<List<Comment>> LoadCommentsFromJsonAsync()
         {
-            string personsJsonString = await DeSerializePersonsFileAsync(jsonFileName);
-            return (List<Person>)JsonConvert.DeserializeObject(personsJsonString, typeof(List<Person>));  
+            string CommentsJsonString = await DeSerializeCommentsFileAsync(jsonFileName);
+            return (List<Comment>)JsonConvert.DeserializeObject(CommentsJsonString, typeof(List<Comment>));  
         }
-        
-       public static async void SerializePersonsFileAsync(string PersonsString, string fileName)
+
+        public static async void SerializeCommentsFileAsync(string CommentsString, string fileName)
         {       
             StorageFile localFile = await ApplicationData.Current.LocalFolder.CreateFileAsync(fileName, CreationCollisionOption.ReplaceExisting);
-            await FileIO.WriteTextAsync(localFile, PersonsString);
+            await FileIO.WriteTextAsync(localFile, CommentsString);
         }
 
-        public static async Task<string> DeSerializePersonsFileAsync(String fileName)
+        public static async Task<string> DeSerializeCommentsFileAsync(String fileName)
         {
             StorageFile localFile = await ApplicationData.Current.LocalFolder.GetFileAsync(fileName);
             return await FileIO.ReadTextAsync(localFile);           
