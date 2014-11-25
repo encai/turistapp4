05c34b3ad35b84628bb223f5b973ebf66f2ca10b
Rasmus Friis <rasmuswfriis@gmail.com>
2014-11-25 11:40:13 +0100

    PersistenceFacade
    gem

---
+++ Turistv4/Model/PersistenceFacade.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using Windows.Storage;
+using Newtonsoft.Json;
+using Turistv4.Model;
+
+namespace WindowsAppsSerializeTest
+{
+    class PersistenceFacade
+    {
+        private static string jsonFileName = "PersonsAsJson.dat";
+        
+        public static async void SavePersonsAsJsonAsync(ObservableCollection<Person> persons)
+        {
+            string personsJsonString = JsonConvert.SerializeObject(persons);
+            SerializePersonsFileAsync(personsJsonString, jsonFileName);
+        }
+
+        public static async Task<List<Comment>> LoadPersonsFromJsonAsync()
+        {
+            string personsJsonString = await DeSerializePersonsFileAsync(jsonFileName);
+            return (List<Person>)JsonConvert.DeserializeObject(personsJsonString, typeof(List<Person>));  
+        }
+        
+       public static async void SerializePersonsFileAsync(string PersonsString, string fileName)
+        {       
+            StorageFile localFile = await ApplicationData.Current.LocalFolder.CreateFileAsync(fileName, CreationCollisionOption.ReplaceExisting);
+            await FileIO.WriteTextAsync(localFile, PersonsString);
+        }
+
+        public static async Task<string> DeSerializePersonsFileAsync(String fileName)
+        {
+            StorageFile localFile = await ApplicationData.Current.LocalFolder.GetFileAsync(fileName);
+            return await FileIO.ReadTextAsync(localFile);           
+        }    
+    }
+}
